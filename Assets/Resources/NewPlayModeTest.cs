7a86f9c2ef2b807d4a7844c18994aef97ae89568
LizaFridman <Liza.fridman39@gmail.com>
2018-01-13 13:20:51 +0200

    Added resources and mini test

---
+++ Assets/Resources/NewPlayModeTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using UnityEditor;
+
+public class NewPlayModeTest {
+
+	[Test]
+	public void NewPlayModeTestSimplePasses() {
+		// Use the Assert class to test conditions.
+	}
+
+	// A UnityTest behaves like a coroutine in PlayMode
+	// and allows you to yield null to skip a frame in EditMode
+	[UnityTest]
+	public IEnumerator NewPlayModeTestWithEnumeratorPasses() {
+		// Use the Assert class to test conditions.
+	    SetupScene();
+		// yield to skip a frame
+		yield return new WaitForSeconds(3);
+	}
+
+    private void SetupScene()
+    {
+        try
+        {
+            var cube = GameObject.Find("BasicCube");
+            PrefabUtility.GetPrefabParent(cube);
+            MonoBehaviour.Instantiate(Resources.Load<GameObject>("GeomagicPen"));
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
