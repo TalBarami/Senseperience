0e17ab1f55140b65d36e325d21b55b17b986920d
TalBarami <talbaramii@gmail.com>
2018-01-14 17:17:59 +0200

    Added sound & objects picking.

---
+++ Assets/Scripts/CollisionSoundController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionSoundController : MonoBehaviour {
+    AudioSource collisionAudio;
+    AudioSource frictionAudio;
+
+    // Use this for initialization
+    void Start()
+    {
+        var audios = GetComponents<AudioSource>();
+        collisionAudio = audios[0];
+        frictionAudio = audios[1];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision Enter");
+        collisionAudio.Play();
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("Collision Stay");
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("Collision Exit");
+    }
+}
---
+++ Assets/Scripts/FlashLightController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLightController : MonoBehaviour {
+    public Light light;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+    bool previousButtonState = false;
+	// Update is called once per frame
+	void Update () {
+        if (!previousButtonState && PluginImport.GetButton2State())
+        {
+            light.enabled = true;
+            previousButtonState = true;
+        } else if(previousButtonState && !PluginImport.GetButton2State())
+        {
+            light.enabled = false;
+            previousButtonState = false;
+        }
+	}
+}
