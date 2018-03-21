2e14aae707148c85e3312f2ff87660b89e24c9b9
LizaFridman <Liza.fridman39@gmail.com>
2018-03-21 19:52:39 +0200

    Expanded tests to sound controller

--- Assets/Scripts/CollisionSoundController.cs
+++ Assets/Scripts/CollisionSoundController.cs
@@ -1,17 +1,44 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using Assets.Util;
+using JetBrains.Annotations;
 using UnityEngine;
 
-public class CollisionSoundController : MonoBehaviour {
-    AudioSource collisionAudio;
-    AudioSource frictionAudio;
+public class CollisionSoundController : MonoBehaviour
+{
+    private AudioSource _collisionAudio;
+    private AudioSource _frictionAudio;
+
+    public AudioSource GetCollisionAudioSource()
+    {
+        return _collisionAudio;
+    }
+
+    public AudioSource GetFrictionAudioSource()
+    {
+        return _frictionAudio;
+    }
+
+    public CollisionState CollisionState
+    {
+        get { return CollisionState; }
+        set
+        {
+            if (!Enum.IsDefined(typeof(CollisionState), value))
+                throw new InvalidEnumArgumentException("value", (int) value, typeof(CollisionState));
+            CollisionState = value;
+        }
+    }
 
     // Use this for initialization
     void Start()
     {
         var audios = GetComponents<AudioSource>();
-        collisionAudio = audios[0];
-        frictionAudio = audios[1];
+        _collisionAudio = audios[0];
+        _frictionAudio = audios[1];
+        CollisionState = CollisionState.None;
     }
 
     // Update is called once per frame
@@ -22,16 +49,19 @@ public class CollisionSoundController : MonoBehaviour {
     void OnCollisionEnter(Collision collision)
     {
         Debug.Log("Collision Enter");
-        collisionAudio.Play();
+        CollisionState = CollisionState.Enter;
+        _collisionAudio.Play();
     }
 
     void OnCollisionStay(Collision collision)
     {
         Debug.Log("Collision Stay");
+        CollisionState = CollisionState.Stay;
     }
 
     void OnCollisionExit(Collision collision)
     {
         Debug.Log("Collision Exit");
+        CollisionState = CollisionState.Exit;
     }
 }
