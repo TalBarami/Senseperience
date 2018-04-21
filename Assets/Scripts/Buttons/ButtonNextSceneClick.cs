bcb7c499e397efd0f8f794799a934a06961a7763
LizaFridman <Liza.fridman39@gmail.com>
2018-04-21 17:35:15 +0300

    added two scenes

--- Assets/Scripts/Buttons/ButtonNextSceneClick.cs
+++ Assets/Scripts/Buttons/ButtonNextSceneClick.cs
@@ -10,7 +10,7 @@ public class ButtonNextSceneClick : ButtonClickScript
         Debug.Log("Starting Game Scene");
         Destroy(GameObject.Find("Geomagic"));
         Destroy(GameObject.Find("GeomagicPen"));
-        SceneManager.LoadScene(1 + (SceneManager.GetActiveScene().buildIndex % 3));
+        SceneManager.LoadScene(((SceneManager.GetActiveScene().buildIndex+1) % 3));
     }
 
     // Use this for initialization
