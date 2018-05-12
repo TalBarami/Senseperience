70cbcd25164cb52f6c350f6195a4ed3c6819c360
LizaFridman <Liza.fridman39@gmail.com>
2018-05-12 16:26:22 +0300

    Changed the logging script
    Not logs for each gameObject in different file

--- Assets/Scripts/Logger/LoggingScript.cs
+++ Assets/Scripts/Logger/LoggingScript.cs
@@ -1,41 +1,69 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using UnityEngine;
 
 namespace Assets.Scripts.Logger
 {
     public class LoggingScript : MonoBehaviour
     {
-        private List<string> _toPersist;
-        private SLogHandler _logHandler;
+        //private List<string> _toPersist;
+        //private SLogHandler _logHandler;
+        private string _logSavePath;
 
+        private StreamWriter _writer;
         // Use this for initialization
         void Start()
         {
-            _logHandler = new SLogHandler();
-            _toPersist = new List<string>();
+            //_logHandler = new SLogHandler();
+            //_toPersist = new List<string>();
+            _logSavePath = Application.dataPath + "/Logs";
+            Debug.Log("Log files path: " + _logSavePath);
+
+            if (!Directory.Exists(Application.dataPath))
+            {
+                Directory.CreateDirectory(_logSavePath);
+            }
+
+            _logSavePath += "/" + name + ".txt";
+            _writer = new StreamWriter(_logSavePath, true);
+
+            _writer.WriteLine("\n\n----------------------------- " + name + " Log -----------------------------");
+            _writer.WriteLine("----------------------------- " + DateTime.Now + " -----------------------------");
+
+            //Application.logMessageReceived += HandleLog;
         }
 
         void FixedUpdate()
         {
-            //Debug.Log("I'm inside " + name + "!");
-            _toPersist.Add("I'm inside " + name + "!");
-        }
-
-        // Update is called once per frame
-        /*void Update()
-        {
-            toPersist.Add("I'm inside "+name+"!");
-        }
-        */
-        void OnDisable()
-        {
-            foreach (var log in _toPersist)
+            if (transform.hasChanged)
             {
-                Debug.Log(log);
+                var dateTime = DateTime.Now;
+                _writer.WriteLine(dateTime + " | "+name+" | Position: " + transform.position);
+
             }
 
-            _logHandler.Disable();
+            //_toPersist.Add("I'm inside " + name + "!");
         }
+
+        
+        void OnDisable()
+        {
+            //Application.logMessageReceived -= HandleLog;
+            _writer.Flush();
+            _writer.Close();
+        }
+        /*
+        void HandleLog(string logString, string stackTrace, LogType type)
+        {
+            using (var writer = new StreamWriter(_logSavePath, true))
+            {
+                var typeCaps = type.ToString().ToUpper();
+                var dateTime = DateTime.Now;
+                writer.WriteLine(dateTime+" [" + typeCaps + "] " + logString);
+            }
+        }*/
     }
 }
 
