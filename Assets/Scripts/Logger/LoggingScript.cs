9017c2426f2c4e088cbeecfc1694547c7de5040c
LizaFridman <Liza.fridman39@gmail.com>
2018-05-20 17:13:51 +0300

    Added Scene and Player Details

--- Assets/Scripts/Logger/LoggingScript.cs
+++ Assets/Scripts/Logger/LoggingScript.cs
@@ -2,36 +2,50 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Assets.Scripts.Logger
 {
     public class LoggingScript : MonoBehaviour
     {
-        //private List<string> _toPersist;
+        private List<string> _toPersist;
+        
+        private Guid _userNameGuid; 
         //private SLogHandler _logHandler;
-        private string _logSavePath;
 
+        public string LogSavePath { get; set; }
+        private string _username;
+
+        public string UserName;
+        
         private StreamWriter _writer;
         // Use this for initialization
         void Start()
         {
             //_logHandler = new SLogHandler();
             //_toPersist = new List<string>();
-            _logSavePath = Application.dataPath + "/Logs";
-            Debug.Log("Log files path: " + _logSavePath);
+            LogSavePath = LogSavePath ?? Application.dataPath + "/Logs";
+            //Debug.Log("Log files path: " + LogSavePath);
 
             if (!Directory.Exists(Application.dataPath))
             {
-                Directory.CreateDirectory(_logSavePath);
+                Directory.CreateDirectory(LogSavePath);
             }
 
-            _logSavePath += "/" + name + ".txt";
-            _writer = new StreamWriter(_logSavePath, true);
+            LogSavePath += "/" + name + ".txt";
 
-            _writer.WriteLine("\n\n----------------------------- " + name + " Log -----------------------------");
-            _writer.WriteLine("----------------------------- " + DateTime.Now + " -----------------------------");
+            _writer = new StreamWriter(LogSavePath, true);
+            _toPersist = new List<string>();
 
+            AddToLog("\n\n----------------------------- " + name + " Log -----------------------------");
+            AddToLog("----------------------------- " + DateTime.Now + " -----------------------------");
+
+            var sceneName = SceneManager.GetActiveScene().name;
+            AddToLog("=> Active Scene: " +sceneName);
+            AddToLog("=> Player: "+GetUserName());
+            AddToLog("--------------------------------------------------------------------------------");
             //Application.logMessageReceived += HandleLog;
         }
 
@@ -40,20 +54,49 @@ namespace Assets.Scripts.Logger
             if (transform.hasChanged)
             {
                 var dateTime = DateTime.Now;
-                _writer.WriteLine(dateTime + " | "+name+" | Position: " + transform.position);
+               AddToLog(dateTime + " | " + name + " | Position: " + transform.position);
 
             }
-
-            //_toPersist.Add("I'm inside " + name + "!");
         }
 
         
         void OnDisable()
         {
             //Application.logMessageReceived -= HandleLog;
+            
+            foreach (var msg in _toPersist)
+            {
+                _writer.WriteLine(msg);
+            }
+
             _writer.Flush();
             _writer.Close();
         }
+
+        private void AddToLog(string msg)
+        {
+            _toPersist.Add(msg);
+            //_writer.WriteLine(msg);
+        }
+
+        private string GenerateUserName()
+        {
+            var newGuid = Guid.NewGuid().ToString();
+            var generatedShortGuid = newGuid.Substring(0, newGuid.Length / 2);
+            return "User_" + generatedShortGuid;
+        }
+
+        public string GetUserName()
+        {
+            if (string.IsNullOrEmpty(_username))
+            {
+                _username = GenerateUserName();
+            }
+
+            UserName = _username;
+            return _username;
+        }
+
         /*
         void HandleLog(string logString, string stackTrace, LogType type)
         {
