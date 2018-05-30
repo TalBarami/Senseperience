d9036ed90272426babc294154252ff84d719c16d
TalBarami <talbaramii@gmail.com>
2018-05-30 17:16:27 +0300

    Added space station & ships to scene2

---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    public Vector3 amplitude;
+    public Vector3 speed;
+    private Vector3 direction;
+    private Vector3 pos0;
+
+    // Use this for initialization
+    void Start()
+    {
+        pos0 = transform.position;
+        direction = new Vector3(1, 1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        Vector3 diff = pos - pos0;
+
+        for(int i=0; i<3; i++)
+        {
+            pos[i] += CalculateDeltaPosition(i, diff[i]);
+        }
+
+        transform.position = pos;
+    }
+
+    float CalculateDeltaPosition(int axis, float diff)
+    {
+        diff *= direction[axis];
+
+        if (diff >= amplitude[axis])
+        {
+            direction[axis] *= -1;
+        }
+
+        return direction[axis] * speed[axis] * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/RotationScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationScript : MonoBehaviour {
+    public Vector3 rotation;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var speed = Random.Range(0f, 1f);
+        transform.Rotate(rotation * speed);
+    }
+    
+}
