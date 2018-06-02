514f2f994c6a06611c5c6ac72df60d22b4f37e44
Carmel Lederer <carmel.lederer@intel.com>
2018-06-02 18:27:41 +0300

    Adding tests
    Boaz new tests added

--- Assets/Scenes/Tests.cs
+++ Assets/Scenes/Tests.cs
@@ -105,7 +105,7 @@ namespace Assets.Scenes
                 if (colDetect != null && colDetect.IsOnCollision)
                 {
                     var otherCollider = colDetect.GetOther();
-                    var cubeType = GameObject.Find("BasicCube").GetType();
+                    var cubeType = GameObject.Find("Prefab").GetType();
                     Assert.AreEqual(cubeType, otherCollider.gameObject.GetType());
                     yield break;
                 }
@@ -177,7 +177,7 @@ namespace Assets.Scenes
             SceneManager.SetActiveScene(SceneManager.GetSceneByName(sceneName));
             yield return new WaitForSeconds(1);
 
-            var cubePrefab = GameObject.Find("BasicCube");
+            var cubePrefab = GameObject.Find("Prefab");
             Assert.NotNull(cubePrefab);
 
             //SceneManager.SetActiveScene(testScene);
@@ -232,7 +232,7 @@ namespace Assets.Scenes
 
             for (var i = 0; i < 5; i++)
             {
-                var cubePrefab = GameObject.Find("BasicCube");
+                var cubePrefab = GameObject.Find("Prefab");
                 yield return new WaitForSeconds(1);
                 var colDetect = cubePrefab.GetComponent<CollisionSoundController>();
                 if (colDetect != null)
@@ -255,13 +255,13 @@ namespace Assets.Scenes
                         case CollisionState.Stay:
                             {
                                 var collisionAudio = colDetect.GetCollisionAudioSource();
-                                Assert.IsFalse(collisionAudio.isPlaying);
+                                Assert.IsTrue(collisionAudio.isPlaying);
                                 break;
                             }
                         case CollisionState.Exit:
                             {
                                 var collisionAudio = colDetect.GetCollisionAudioSource();
-                                Assert.IsFalse(collisionAudio.isPlaying);
+                                Assert.IsTrue(collisionAudio.isPlaying);
                                 break;
 
                             }
@@ -276,7 +276,7 @@ namespace Assets.Scenes
         [UnityTest]
         public IEnumerator TestCollisionSoundOnSphere()
         {
-            const string sceneName = "TestSphere";
+            const string sceneName = "TestBox";
             yield return SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
 
             var testScene = SceneManager.GetSceneByName(sceneName);
@@ -286,7 +286,7 @@ namespace Assets.Scenes
 
             for (var i = 0; i < 5; i++)
             {
-                var cubePrefab = GameObject.Find("Sphere");
+                var cubePrefab = GameObject.Find("Prefab");
                 yield return new WaitForSeconds(1);
                 var colDetect = cubePrefab.GetComponent<CollisionSoundController>();
                 if (colDetect != null)
@@ -309,21 +309,270 @@ namespace Assets.Scenes
                         case CollisionState.Stay:
                             {
                                 var collisionAudio = colDetect.GetCollisionAudioSource();
-                                Assert.IsFalse(collisionAudio.isPlaying);
+                                Assert.IsTrue(collisionAudio.isPlaying);
                                 break;
                             }
                         case CollisionState.Exit:
                             {
                                 var collisionAudio = colDetect.GetCollisionAudioSource();
-                                Assert.IsFalse(collisionAudio.isPlaying);
+                                Assert.IsTrue(collisionAudio.isPlaying);
                                 break;
+
                             }
                     }
                     break;
                 }
             }
-            Assert.Fail("Nothing Found");
+
             yield return SceneManager.UnloadSceneAsync(sceneName);
         }
+
+        public IEnumerator CursorCollidesInButtonScene()
+        {
+            const string sceneName = "TestBox";
+           // var activeScene = SceneManager.GetActiveScene();
+
+            yield return SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
+
+            var testScene = SceneManager.GetSceneByName(sceneName);
+            SceneManager.SetActiveScene(testScene);
+
+            yield return new WaitForSeconds(1);
+
+            for (var i = 0; i < 5; i++)
+            {
+                var cubePrefab = GameObject.Find("GeomagicPen");
+                yield return new WaitForSeconds(1);
+                var colDetect = cubePrefab.GetComponent<CollisionDetection>();
+                if (colDetect != null)
+                {
+                    Assert.IsTrue(colDetect.IsOnCollision, "Collision Detection Object: " + colDetect +
+                                                           "\nisCollision = " + colDetect.IsOnCollision);
+                    break;
+                }
+                else
+                {
+                    Assert.Fail("Collision Detection Object: " + colDetect);
+                }
+            }
+
+            //yield return SceneManager.LoadSceneAsync(activeScene.name);
+            //SceneManager.SetActiveScene(activeScene);
+            yield return SceneManager.UnloadSceneAsync(sceneName);
+        }
+
+
+         [UnityTest]
+        public IEnumerator CursorCollidesWithButton()
+        {
+            const string sceneName = "TestBox";
+            //var activeScene = SceneManager.GetActiveScene();
+
+            yield return SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
+
+            var testScene = SceneManager.GetSceneByName(sceneName);
+            SceneManager.SetActiveScene(testScene);
+
+            yield return new WaitForSeconds(1);
+
+            for (var i = 0; i < 5; i++)
+            {
+                var cursorPrefab = GameObject.Find("GeomagicPen");
+                yield return new WaitForSeconds(1);
+
+                var colDetect = cursorPrefab.GetComponent<CollisionDetection>();
+                if (colDetect != null && colDetect.IsOnCollision)
+                {
+                    var otherCollider = colDetect.GetOther();
+                    var cubeType = GameObject.Find("Prefab").GetType();
+                    Assert.AreEqual(cubeType, otherCollider.gameObject.GetType());
+                    yield break;
+                }
+                else
+                {
+                    Assert.Fail("component = "+colDetect);
+                }
+            }
+            //yield return SceneManager.LoadSceneAsync(activeScene.name);
+            //SceneManager.SetActiveScene(activeScene);
+            yield return SceneManager.UnloadSceneAsync(sceneName);
+        }
+
+        [UnityTest]
+        public IEnumerator CursorCollidesWithRoom()
+        {
+            const string sceneName = "TestRoom";
+           // var testScene = SceneManager.GetActiveScene();
+
+            yield return SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
+            SceneManager.SetActiveScene(SceneManager.GetSceneByName(sceneName));
+            yield return new WaitForSeconds(1);
+
+            for (var i = 0; i < 5; i++)
+            {
+                var cursorPrefab = GameObject.Find("GeomagicPen");
+                yield return new WaitForSeconds(1);
+
+                var colDetect = cursorPrefab.GetComponent<CollisionDetection>();
+                if (colDetect != null && colDetect.IsOnCollision)
+                {
+                    var otherCollider = colDetect.GetOther();
+                    var sphereType = GameObject.Find("Floor").GetType();
+                    Assert.AreEqual(sphereType, otherCollider.gameObject.GetType());
+                    yield break;
+                }
+                else
+                {
+                    Assert.Fail("component = " + colDetect);
+                }
+            }
+
+            //SceneManager.SetActiveScene(testScene);
+            yield return SceneManager.UnloadSceneAsync(sceneName);
+        }
+
+        [UnityTest]
+        public IEnumerator TestButtonSceneLoading()
+        {
+            const string sceneName = "TestButton";
+            //var testScene = SceneManager.GetActiveScene();
+            
+            yield return SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
+            SceneManager.SetActiveScene(SceneManager.GetSceneByName(sceneName));
+
+            Assert.AreEqual(sceneName, SceneManager.GetActiveScene().name);
+
+            //SceneManager.SetActiveScene(testScene);
+            yield return SceneManager.UnloadSceneAsync(sceneName);
+
+        }
+
+        [UnityTest]
+        public IEnumerator TestButtonPrefabLoading()
+        {
+            var sceneName = "TestSphere";
+            //var testScene = SceneManager.GetActiveScene();
+            yield return SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
+            SceneManager.SetActiveScene(SceneManager.GetSceneByName(sceneName));
+            yield return new WaitForSeconds(1);
+
+            var cubePrefab = GameObject.Find("Sphere");
+            Assert.NotNull(cubePrefab);
+
+            //SceneManager.SetActiveScene(testScene);
+            yield return SceneManager.UnloadSceneAsync(sceneName);
+        }
+
+        [UnityTest]
+        public IEnumerator TestButtonBackgroundMusicLoading()
+        {
+            var sceneName = "TestMusic";
+            //var testScene = SceneManager.GetActiveScene();
+            yield return SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
+            SceneManager.SetActiveScene(SceneManager.GetSceneByName(sceneName));
+            yield return new WaitForSeconds(1);
+
+            var cubePrefab = GameObject.Find("BackgroundMusic");
+            Assert.NotNull(cubePrefab);
+
+            //SceneManager.SetActiveScene(testScene);
+            yield return SceneManager.UnloadSceneAsync(sceneName);
+        }
+                
+        [UnityTest]
+        public IEnumerator TestDecorations()
+        {
+            var sceneName = "TestDecoration";
+            //var testScene = SceneManager.GetActiveScene();
+            yield return SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
+            SceneManager.SetActiveScene(SceneManager.GetSceneByName(sceneName));
+            yield return new WaitForSeconds(1);
+
+            var cubePrefab = GameObject.Find("Picture");
+            Assert.NotNull(cubePrefab);
+
+            var cubePrefab = GameObject.Find("Saw");
+            Assert.NotNull(cubePrefab);
+
+            var cubePrefab = GameObject.Find("Sylvanas");
+            Assert.NotNull(cubePrefab);
+
+            //SceneManager.SetActiveScene(testScene);
+            yield return SceneManager.UnloadSceneAsync(sceneName);
+        }
+        [UnityTest]
+        public IEnumerator TestRoomPrefabLoading()
+        {
+            var sceneName = "TestSphere";
+            //var testScene = SceneManager.GetActiveScene();
+            yield return SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
+            SceneManager.SetActiveScene(SceneManager.GetSceneByName(sceneName));
+            yield return new WaitForSeconds(1);
+
+            var spherePrefab = GameObject.Find("Floor");
+            Assert.NotNull(spherePrefab);
+
+            //SceneManager.SetActiveScene(testScene);
+            yield return SceneManager.UnloadSceneAsync(sceneName);
+        }
+
+
+
+        [UnityTest]
+        public IEnumerator TestCollisionSoundOnButton()
+        {
+            const string sceneName = "TestBox";
+            yield return SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
+
+            var testScene = SceneManager.GetSceneByName(sceneName);
+            SceneManager.SetActiveScene(testScene);
+
+            yield return new WaitForSeconds(1);
+
+            for (var i = 0; i < 5; i++)
+            {
+                var cubePrefab = GameObject.Find("Prefab");
+                yield return new WaitForSeconds(1);
+                var colDetect = cubePrefab.GetComponent<CollisionSoundController>();
+                if (colDetect != null)
+                {
+                    var colState = colDetect.GetCollisionState();
+                    switch (colState)
+                    {
+                        case CollisionState.None:
+                            {
+                                var collisionAudio = colDetect.GetCollisionAudioSource();
+                                Assert.IsFalse(collisionAudio.isPlaying);
+                                break;
+                            }
+                        case CollisionState.Enter:
+                            {
+                                var collisionAudio = colDetect.GetCollisionAudioSource();
+                                Assert.IsTrue(collisionAudio.isPlaying);
+                                break;
+                            }
+                        case CollisionState.Stay:
+                            {
+                                var collisionAudio = colDetect.GetCollisionAudioSource();
+                                Assert.IsTrue(collisionAudio.isPlaying);
+                                break;
+                            }
+                        case CollisionState.Exit:
+                            {
+                                var collisionAudio = colDetect.GetCollisionAudioSource();
+                                Assert.IsTrue(collisionAudio.isPlaying);
+                                break;
+
+                            }
+                    }
+                    break;
+                }
+            }
+
+            yield return SceneManager.UnloadSceneAsync(sceneName);
+        }
+
+        
     }
+    
 }
