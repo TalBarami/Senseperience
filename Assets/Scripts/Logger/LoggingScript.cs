a6820a2374c360f98808692b6c28a91f4eaa1a1f
LizaFridman <Liza.fridman39@gmail.com>
2018-06-05 15:37:16 +0300

    Drawing path - added MonoBehavior and Editor

--- Assets/Scripts/Logger/LoggingScript.cs
+++ Assets/Scripts/Logger/LoggingScript.cs
@@ -10,7 +10,6 @@ namespace Assets.Scripts.Logger
 {
     public class LoggingScript : MonoBehaviour
     {
-        private const string PositionPrefix = "Position Changed: ";
         private readonly TimeSpan _timeInterval = TimeSpan.FromMilliseconds(50);
         private List<string> _toPersist;
         private StreamWriter _writer;
@@ -27,20 +26,11 @@ namespace Assets.Scripts.Logger
 
         private string SceneName;
 
-        /*void OnEnable() {
-            SceneManager.sceneLoaded += OnSceneFinishedLoading;
-        }
-
-        void OnSceneFinishedLoading(Scene scene, LoadSceneMode mode)
-        {
-            Debug.Log("Scene" + scene.name + " Loaded");
-        }*/
-
         // Use this for initialization
         void Start()
         {
             _toPersist = new List<string>();
-            _writer = new StreamWriter(GetLogPath(), true);
+            _writer = new StreamWriter(GetLogFilePath(), true);
             _lastWrittenDateTime = DateTime.MinValue;
             _lastWrittenPosition = transform.position;
 
@@ -86,24 +76,11 @@ namespace Assets.Scripts.Logger
             AddToLog("\n----------------------------- End Of Log -----------------------------");
 
             FlushToFile();
-            /*
-            var vectors = ReadPositionsFromLogFile();
-            _writer = new StreamWriter(GetLogPath(), true);
-            _toPersist.Clear();
-
-            for (int i = 0; i < vectors.Length; i++)
-            {
-                if (vectors[i] != default(Vector3) || vectors[i] == new Vector3(0, 0, 0))
-                {
-                    AddToLog("Found Position: " + vectors[i]);
-                }
-            }
-
-            FlushToFile();*/
         }
 
         private void WritePositionToLog(Vector3 position, DateTime logTime) {
-            AddToLog(PositionPrefix + position, logTime);
+            AddToLog(LoggingUtils.PositionPrefix + LoggingUtils.Vector3toString(position), logTime);
+
             _lastWrittenDateTime = logTime;
             _lastWrittenPosition = position;
             transform.hasChanged = false;
@@ -145,61 +122,6 @@ namespace Assets.Scripts.Logger
             //_writer.WriteLine(msg);
         }
 
-        private Vector3[] ReadPositionsFromLogFile()
-        {
-            StreamReader reader = new StreamReader(GetLogPath());
-            var inputFile = reader.ReadToEnd();
-            var lines = ExtractPositionStrings(inputFile.Split("\n"[0]));
-            
-            Vector3[] vectors = new Vector3[lines.Length];
-
-            for (int i = 0; i < lines.Length; i++)
-            {
-                var current = lines[i];
-                //Debug.Log("Line " + i + ": " + current);
-                //Debug.Log("Line length = " + current.Length);
-                vectors[i] = StringToVector3(current);
-                //Debug.Log("Read the following vector: " + vectors[i]);
-
-
-            }
-
-            reader.Close();
-            return vectors;
-        }
-
-        private static Vector3 StringToVector3(string sVector)
-        {
-            if (sVector.Length < 3) {
-                return default(Vector3);
-            }
-
-            //Debug.Log("Removing parentheses");
-            sVector = sVector.Substring(1, sVector.Length - 3);
-            //Debug.Log("Splitting: "+sVector);
-            string[] sArray = sVector.Split(',');
-
-            return new Vector3(
-                float.Parse(sArray[0]),
-                float.Parse(sArray[1]),
-                float.Parse(sArray[2]));
-        }
-
-        private string[] ExtractPositionStrings(string[] lines)
-        {
-            var extracted = new string[lines.Length];
-            for (int i = 0; i < lines.Length; i++)
-            {
-                extracted[i] = string.Empty;
-                var startOfPrefix = lines[i].IndexOf(PositionPrefix);
-                if (startOfPrefix < 0) continue;
-                var lengthToRead = lines[i].Length - (startOfPrefix + PositionPrefix.Length);
-                extracted[i] = (lines[i].Substring(startOfPrefix + PositionPrefix.Length, lengthToRead));
-            }
-
-            return extracted;
-        }
-
         private string GenerateUserName()
         {
             var newGuid = Guid.NewGuid().ToString();
@@ -221,28 +143,20 @@ namespace Assets.Scripts.Logger
             return UserName;
         }
 
-        private string GetLogPath()
+        public string GetLogFilePath()
         {
             if (string.IsNullOrEmpty(LogSavePath))
             {
                 LogSavePath = Application.dataPath + "/Logs";
                 //Debug.Log("Log files path: " + LogSavePath);
 
-                CreateDirectoryPathIfMissing(LogSavePath);
+                LoggingUtils.CreateDirectoryPathIfMissing(LogSavePath);
                 CreateFileNameIfMissing();
                
                 LogSavePath += "/" + FileName + ".txt";
             }
             
             return LogSavePath;
-        }
-
-
-        void CreateDirectoryPathIfMissing(string path) {
-            if (!Directory.Exists(path))
-            {
-                Directory.CreateDirectory(path);
-            }
         }
 
         void CreateFileNameIfMissing() {
